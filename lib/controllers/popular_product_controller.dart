4a9ab8218664e70361f18b4241e12080c18dd9d3
Arda Demirel <yademirel21@gmail.com>
2023-02-19 02:49:30 +0300

    Started to develop api connections

---
+++ lib/controllers/popular_product_controller.dart
@@ -0,0 +1,20 @@
+import 'package:food_delivery/data/repository/popular_procuct_repo.dart';
+import 'package:get/get.dart';
+
+class PopularProductController extends GetxController{
+  final PopularProductRepo popularProductRepo;
+  PopularProductController({required this.popularProductRepo});
+  List<dynamic> _popularProductList=[];
+  List<dynamic> get popularProductList => _popularProductList;
+
+  Future<void>getPopularProductList()async {
+    Response response = await popularProductRepo.getPopularProductList();
+    if(response.statusCode==200){ //call successful
+      _popularProductList=[];
+      //_popularProductList.addAll();
+      update();
+    }else{
+
+    }
+  }
+}
