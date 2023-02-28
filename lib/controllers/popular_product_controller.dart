70ce1fb484b6cbb2e99f920c0e0eb7c8064adbc8
Arda Demirel <yademirel21@gmail.com>
2023-02-28 23:21:22 +0300

    Added a counter to the cart icon on the top-right

--- lib/controllers/popular_product_controller.dart
+++ lib/controllers/popular_product_controller.dart
@@ -6,97 +6,93 @@ import 'package:get/get.dart';
 import '../models/products_model.dart';
 import 'cart_controller.dart';
 
-class PopularProductController extends GetxController{
+class PopularProductController extends GetxController {
   final PopularProductRepo popularProductRepo;
+
   PopularProductController({required this.popularProductRepo});
-  List<dynamic> _popularProductList=[];
+
+  List<dynamic> _popularProductList = [];
+
   List<dynamic> get popularProductList => _popularProductList;
   late CartController _cart;
 
   bool _isLoaded = false;
-  bool get isLoaded=>_isLoaded;
 
-  int _quantity=0;
-  int get quantity=>_quantity;
-  int _inCartItems=0;
-  int get inCartItems=>_inCartItems+_quantity;
+  bool get isLoaded => _isLoaded;
 
-  Future<void>getPopularProductList()async {
+  int _quantity = 0;
+
+  int get quantity => _quantity;
+  int _inCartItems = 0;
+
+  int get inCartItems => _inCartItems + _quantity;
+
+  Future<void> getPopularProductList() async {
     Response response = await popularProductRepo.getPopularProductList();
-    if(response.statusCode==200){ //call successful
+    if (response.statusCode == 200) {
+      //call successful
       print("got products");
-      _popularProductList=[];
-      _popularProductList.addAll(Product.fromJson(response.body).products as Iterable);
+      _popularProductList = [];
+      _popularProductList
+          .addAll(Product.fromJson(response.body).products as Iterable);
       //print(_popularProductList);
       _isLoaded = true;
       update();
-
-    }else{
-
-    }
+    } else {}
   }
 
-  void setQuantity(bool isIncrement){
-    if(isIncrement){
-      _quantity = checkQuantity(_quantity+1);
-    }
-    else{
-      _quantity =checkQuantity(_quantity-1);
+  void setQuantity(bool isIncrement) {
+    if (isIncrement) {
+      _quantity = checkQuantity(_quantity + 1);
+    } else {
+      _quantity = checkQuantity(_quantity - 1);
     }
     update(); //update UI
   }
 
-  int checkQuantity(int quantity){
-    if((_inCartItems+quantity)<0){
+  int checkQuantity(int quantity) {
+    if ((_inCartItems + quantity) < 0) {
       Get.snackbar("Item Count", "You can't reduce more!",
-      backgroundColor: AppColors.mainColor,
-      colorText: Colors.white);
+          backgroundColor: AppColors.mainColor, colorText: Colors.white);
       return 0;
-    }
-    else if((_inCartItems+quantity)>20){ //maximum quantity
+    } else if ((_inCartItems + quantity) > 20) {
+      //maximum quantity
       Get.snackbar("Item Count", "You can't add more!",
-          backgroundColor: AppColors.mainColor,
-          colorText: Colors.white
-      );
+          backgroundColor: AppColors.mainColor, colorText: Colors.white);
       return 20;
-    }
-    else{
+    } else {
       return quantity;
     }
   }
 
-  void initQuantity(ProductModel product,CartController cart){
+  void initQuantity(ProductModel product, CartController cart) {
     _quantity = 0;
     _inCartItems = 0;
     _cart = cart;
     var exist = false;
     exist = _cart.existInCart(product);
-    print("exist = "+exist.toString());
-    if(exist){
-      _inCartItems=_cart.getQuantity(product);
+    print("exist = " + exist.toString());
+    if (exist) {
+      _inCartItems = _cart.getQuantity(product);
       print(_inCartItems);
     }
   }
 
-  void addItem(ProductModel product){
-    print("quantity = "+quantity.toString());
-    //if(quantity>0){
-      _cart.addItem(product, _quantity);
-      _quantity = 0;
-      _inCartItems=_cart.getQuantity(product);
-      _cart.items.forEach((key, value) {
-        print("The id is "+value.id.toString()+" The quantity is "+value.quantity.toString());
-      });
-      /*
-    }else{
-      Get.snackbar("Item Count", "You should at least add 1 item in the cart!",
-          backgroundColor: AppColors.mainColor,
-          colorText: Colors.white);
-    }
-
-
-       */
-
+  void addItem(ProductModel product) {
+    print("quantity = " + quantity.toString());
+    _cart.addItem(product, _quantity);
+    _quantity = 0;
+    _inCartItems = _cart.getQuantity(product);
+    _cart.items.forEach((key, value) {
+      print("The id is " +
+          value.id.toString() +
+          " The quantity is " +
+          value.quantity.toString());
+    });
+    update(); //UI'da değişiklik olacak(cart iconu üzerinde item sayısını gösteren container oluşturulacak.)
   }
 
+  int get totalItems {
+    return _cart.totalItems;
+  }
 }
--- lib/pages/food/popular_food_detail.dart
+++ lib/pages/food/popular_food_detail.dart
@@ -59,7 +59,31 @@ class PopularFoodDetail extends StatelessWidget {
                     },
                       child:
                   AppIcon(icon: Icons.arrow_back_ios)),
-                  AppIcon(icon: Icons.shopping_cart_outlined)
+
+                  GetBuilder<PopularProductController>(builder: (controller){
+                      return Stack(
+                        children: [
+                          AppIcon(icon: Icons.shopping_cart_outlined),
+                          Get.find<PopularProductController>().totalItems>=1?
+                            Positioned(
+                              right:0,top:0,
+                              child: AppIcon(icon: Icons.circle,
+                                size: 20,
+                                iconColor: Colors.transparent,
+                                backgroundColor: AppColors.mainColor,),
+                            ):
+                              Container(),
+                          Get.find<PopularProductController>().totalItems>=1?
+                          Positioned(
+                            right:Dimensions.width5,top:3,
+                            child: BigText(text: Get.find<PopularProductController>().totalItems.toString(),
+                            size: 12, color: Colors.white,)
+                          ):
+                          Container()
+                        ],
+                      );
+                  })
+
                 ],
 
           )),
