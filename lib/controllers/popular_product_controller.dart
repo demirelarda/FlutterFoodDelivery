ebbd0111a87fca6a6a932b7b242759c690c16d32
Arda Demirel <yademirel21@gmail.com>
2023-03-01 01:00:56 +0300

    Fixed a bug that causes issues for cart logic

--- lib/controllers/popular_product_controller.dart
+++ lib/controllers/popular_product_controller.dart
@@ -54,6 +54,10 @@ class PopularProductController extends GetxController {
     if ((_inCartItems + quantity) < 0) {
       Get.snackbar("Item Count", "You can't reduce more!",
           backgroundColor: AppColors.mainColor, colorText: Colors.white);
+      if(_inCartItems>0){
+        _quantity = -_inCartItems;
+        return _quantity;
+      }
       return 0;
     } else if ((_inCartItems + quantity) > 20) {
       //maximum quantity
